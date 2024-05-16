5ae7c518938bff4203dbe15f1edcfb18773484e3
raffiadiprana <raffiadiprana469@gmail.com>
2024-05-16 21:53:05 +0700

    update

--- app/Views/login_page.php
+++ app/Views/login_page.php
@@ -28,17 +28,14 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-md-8 col-sm-10">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             <!-- Replace the background image class with the new image -->
-                            <div class="col-lg-6 d-none d-lg-block bg-psychology-image">
-                                <img src="" alt="" srcset="">
-                            </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
